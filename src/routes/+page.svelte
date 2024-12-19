72aac9c9aaef4345ae9550ed295e2f312d5218a1
John Zeng <johnzeng878@gmail.com>
2024-12-18 16:04:43 -0800

    add css to organize the views accurately

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,10 +27,50 @@
 	});
 </script>
 
-<div>
-	<ControlPanel/>
-	<CameraDisplay cameraId={$top} windowName='Top View' winId={0}/>
-	<CameraDisplay cameraId={$front} windowName='Front View' winId={1}/>
-	<CameraDisplay cameraId={$left} windowName='Left View' winId={2}/>
+<div class="grid-container">
+    <div class="control-panel">
+        <ControlPanel />
+    </div>
+    <div class="camera-display top">
+        <CameraDisplay cameraId={$top} windowName="Top View" winId={0} />
+    </div>
+    <div class="camera-display front">
+        <CameraDisplay cameraId={$front} windowName="Front View" winId={1} />
+    </div>
+    <div class="camera-display left">
+        <CameraDisplay cameraId={$left} windowName="Left View" winId={2} />
+    </div>
 </div>
 
+<style>
+.grid-container {
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
+    height: 100vh; /* Adjust to fit the viewport */
+}
+
+.control-panel {
+    grid-row: 1 / 2; /* Top-left */
+    grid-column: 1 / 2;
+    background-color: #f0f0f0; /* Optional: style for visual distinction */
+}
+
+.camera-display.top {
+    grid-row: 1 / 2; /* Top-right */
+    grid-column: 2 / 3;
+    background-color: #d0d0f0; /* Optional */
+}
+
+.camera-display.front {
+    grid-row: 2 / 3; /* Bottom-right */
+    grid-column: 2 / 3;
+    background-color: #f0d0d0; /* Optional */
+}
+
+.camera-display.left {
+    grid-row: 2 / 3; /* Bottom-left */
+    grid-column: 1 / 2;
+    background-color: #d0f0d0; /* Optional */
+}
+</style>
